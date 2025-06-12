90794567a2bc516402fd0522f9b0d14e853d91fe
Antoine Galy <antoine.galy.lamon@gmail.com>
2025-06-12 05:41:17 +0200

    caca

--- src/pages/script.js
+++ src/pages/script.js
@@ -3,10 +3,10 @@ import Socket from '@/scripts/Socket.js'
 
 const experience = new Experience(document.querySelector('canvas#webgl'))
 
-new Socket().connect('game')
+const socket = new Socket().connect('game')
 
 if (!window.location.hash.includes('debug')) {
-    // select webgl canvas and add cursor none
     const canvas = document.querySelector('canvas#webgl')
     canvas.style.cursor = 'none'
 }
+
